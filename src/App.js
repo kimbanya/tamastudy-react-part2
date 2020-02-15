848fa50c2d9caab057c7bd7c7d63e0d24ede3dfa
shiningby <kimbanya@gmail.com>
2020-02-15 16:37:10 +0900

    0215 review

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import Home from './pages/Home';
+import Tama from './pages/Tama';
+import GlobalHeader from './components/global/GlobalHeader';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <GlobalHeader />
+        <Switch>
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/tama1" component={Tama} />
+          <Route exact path="/test" component={() => <div>test</div>} />
+          <Redirect from="*" to="/" />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
